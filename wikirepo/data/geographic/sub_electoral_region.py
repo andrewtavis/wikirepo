e689c9f6645c68c2f39a0049dfa9beeaa21d6b62
Andrew Tavis <andrew.t.mcallister@gmail.com>
2021-02-03 17:42:04 +0100

    Add outline of electoral region property

---
+++ wikirepo/data/geographic/sub_electoral_region.py
@@ -0,0 +1,23 @@
+"""
+Functions querying sub_electoral_region information
+
+Ex: US congressional voting districts are sub_electoral_regions of states
+
+This is done via 'PXYZ' (contains electoral district) applied to any level
+Explicitly used when the there are different electoral and administrative sub_regions
+
+Contents
+--------
+    0. No Class
+        query_prop_data
+"""
+
+# pid = 'PXYZ'
+# sub_pid = None
+# col_name = 'electoral_reg'
+# col_prefix = None
+# ignore_char = ''
+# span = True
+
+# def query_prop_data():
+#     return
